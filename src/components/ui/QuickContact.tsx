a80202474aae7f10c8678ab6ff2d71391f3a2067
davistheweb <josiahdave001@gmail.com>
2026-05-14 07:18:40 +0100

    codebase refactor

--- src/components/ui/QuickContact.tsx
+++ src/components/ui/QuickContact.tsx
@@ -2,29 +2,27 @@ import { rubikFont } from "@/lib/font";
 import { Mail, Phone } from "lucide-react";
 import React from "react";
 
-export const QuickContact: React.FC = () => {
-  return (
-    <div className="flex w-full items-center bg-black px-8 py-2">
-      <li className="flex flex-col gap-1 text-white sm:flex-row sm:gap-5">
-        <a
-          href="tel:+2349076626835"
-          className={`${rubikFont.className} flex items-center gap-2 text-[14px] font-medium`}
-        >
-          <Phone
-            fill="white"
-            size={14}
-          />{" "}
-          +234 907 662 6835
-        </a>
-        <a
-          href="mailto:nacosimsu@gmail.com"
-          target="_blank"
-          className={`${rubikFont.className} flex items-center gap-2 text-[14px] font-medium`}
-        >
-          <Mail size={14} />
-          nacosimsu@gmail.com
-        </a>
-      </li>
-    </div>
-  );
-};
+export const QuickContact: React.FC = () => (
+  <div className="flex w-full items-center bg-black px-8 py-2">
+    <li className="flex flex-col gap-1 text-white sm:flex-row sm:gap-5">
+      <a
+        href="tel:+2349076626835"
+        className={`${rubikFont.className} flex items-center gap-2 text-[14px] font-medium`}
+      >
+        <Phone
+          fill="white"
+          size={14}
+        />{" "}
+        +234 907 662 6835
+      </a>
+      <a
+        href="mailto:nacosimsu@gmail.com"
+        target="_blank"
+        className={`${rubikFont.className} flex items-center gap-2 text-[14px] font-medium`}
+      >
+        <Mail size={14} />
+        nacosimsu@gmail.com
+      </a>
+    </li>
+  </div>
+);
