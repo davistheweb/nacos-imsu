4abac2e8e7f40409b9c245cebf25bf32dc8239c5
davistheweb <josiahdave001@gmail.com>
2026-05-14 15:06:08 +0100

    :

--- src/app/association/page.tsx
+++ src/app/association/page.tsx
@@ -1,9 +1,9 @@
-import { AboutAssociation } from "@/components/association/AboutAssociation";
+import { AboutAssociationHeading } from "@/components/association/AboutAssociationHeading";
 
 export default function AssociationPage() {
   return (
     <main className="h-full w-full overflow-x-hidden">
-      <AboutAssociation />
+      <AboutAssociationHeading />
     </main>
   );
 }
--- src/components/association/AboutAssociationHeading.tsx
+++ src/components/association/AboutAssociationHeading.tsx
@@ -1,7 +1,7 @@
 import { nunitoFont } from "@/lib/font";
 import React from "react";
 
-export const AboutAssociation: React.FC = () => (
+export const AboutAssociationHeading: React.FC = () => (
   <section className="about-association-bg flex h-80 items-end justify-center py-5">
     <h1
       className={`text-white ${nunitoFont.className} text-2xl font-extrabold md:text-5xl`}
