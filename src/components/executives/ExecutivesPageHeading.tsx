606b79d09ef10f95b0f2a97c1b696506d9a960b2
davistheweb <josiahdave001@gmail.com>
2026-05-22 22:28:27 +0100

    :

--- src/components/executives/ExecutivesPageHeading.tsx
+++ src/components/executives/ExecutivesPageHeading.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export const ExecutivesPageHeading: React.FC = () => (
   <section className="executives-bg flex h-70 items-center justify-center py-3 lg:items-end">
     <h1
-      className={`text-white ${nunitoFont.className} text-2xl font-extrabold md:text-5xl`}
+      className={`text-white ${nunitoFont.className} text-center text-2xl font-extrabold md:text-5xl`}
     >
       Executive Council
     </h1>
