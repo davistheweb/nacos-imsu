83d0dd99afc13737e1dff92afdeed767857b3f4f
davistheweb <josiahdave001@gmail.com>
2026-05-22 22:57:27 +0100

    :

--- src/components/executives/ExecutivesPageHeading.tsx
+++ src/components/executives/ExecutivesPageHeading.tsx
@@ -2,7 +2,7 @@ import { nunitoFont } from "@/lib/font";
 import React from "react";
 
 export const ExecutivesPageHeading: React.FC = () => (
-  <section className="executives-bg flex h-70 items-center justify-center py-3 lg:items-end">
+  <section className="executives-bg flex h-60 items-center justify-center lg:items-end">
     <h1
       className={`text-white ${nunitoFont.className} text-center text-2xl font-extrabold md:text-5xl`}
     >
