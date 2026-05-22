aa5ac04725f37a2aea0a5fe3dcc094cb847bf45a
davistheweb <josiahdave001@gmail.com>
2026-05-22 22:28:14 +0100

    :

--- src/components/chapter-presidents/ChapterPresidentsHeading.tsx
+++ src/components/chapter-presidents/ChapterPresidentsHeading.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export const ChapterPresidentsHeading: React.FC = () => (
   <section className="chapter-presidents-bg flex h-80 items-center justify-center py-5 lg:items-end">
     <h1
-      className={`text-white ${nunitoFont.className} text-2xl font-extrabold md:text-5xl`}
+      className={`text-white ${nunitoFont.className} text-center text-2xl font-extrabold md:text-5xl`}
     >
       CHAPTER PRESIDENTS (15TH REPUBLIC TILL DATE)
     </h1>
