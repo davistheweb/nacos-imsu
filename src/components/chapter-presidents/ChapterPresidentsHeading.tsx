6cbaddaba11b87ac1fea6e6f4604f7ae9c12b91e
davistheweb <josiahdave001@gmail.com>
2026-06-07 00:57:17 +0100

    :

--- src/components/chapter-presidents/ChapterPresidentsHeading.tsx
+++ src/components/chapter-presidents/ChapterPresidentsHeading.tsx
@@ -2,11 +2,13 @@ import { nunitoFont } from "@/lib/font";
 import React from "react";
 
 export const ChapterPresidentsHeading: React.FC = () => (
-  <section className="chapter-presidents-bg flex h-80 w-full flex-col items-center justify-center py-5 lg:h-100 lg:items-center">
+  <section className="chapter-presidents-bg flex h-80 w-full flex-col items-center justify-center py-5 lg:h-100 lg:items-center gap-1">
     <h1
       className={`text-white ${nunitoFont.className} text-center text-2xl font-extrabold md:text-5xl`}
     >
-      Chapter Presidents (15th Republic Till Date)
+    
+      Chapter Presidents
     </h1>
+    <h1>(15th Republic Till Date)</h1>
   </section>
 );
