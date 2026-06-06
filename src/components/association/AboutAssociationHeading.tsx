272861cba22bafe0f5a3f1b271bf8aa878fee20f
davistheweb <josiahdave001@gmail.com>
2026-06-07 00:12:43 +0100

    :

--- src/components/association/AboutAssociationHeading.tsx
+++ src/components/association/AboutAssociationHeading.tsx
@@ -2,7 +2,7 @@ import { nunitoFont } from "@/lib/font";
 import React from "react";
 
 export const AboutAssociationHeading: React.FC = () => (
-  <section className="about-association-bg flex flex-col gap-1 h-75 items-center justify-center pt-10 py-5 lg:items-end">
+  <section className="about-association-bg flex flex-col gap-1 h-60 items-center justify-end pt-10 py-5 lg:items-end">
     <h1
       className={`text-white ${nunitoFont.className} text-2xl font-extrabold md:text-5xl`}
     >
